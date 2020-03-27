6fbf7577b9027197a75e66e2a0729fdbefbea414
ido-postelnik <postelnik@gmail.com>
2020-03-27 09:18:15 +0300

    [src] sketch workflow mode - in progress

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,87 @@
 <template>
-  <div class="home">
-
+  <div
+    class="home"
+    :class="{
+      'sketch-mode': activeWorkFlowMode === 'SKETCH',
+      'wireframe-mode': activeWorkFlowMode === 'WIREFRAME',
+      'code-mode': activeWorkFlowMode === 'CODE'}">
     <!-- main section -->
     <div class="main-container flex layout-align-start-center">
       <div class="sky"></div>
       <div class="sun"></div>
       <div class="road"></div>
       <div class="arrow-down clickable" @click="onArrowDownClick()">
-        <img src="../assets/icons/arrow-down.svg" alt="arrow-down" height="20"/>
+        <img
+          src="../assets/icons/arrow-down.svg"
+          alt="arrow-down"
+          height="20"/>
       </div>
 
       <!-- ido-postelnik -->
-      <div class="hero-container" :style="{'margin-top': heroContainerMargin + '%'}">
+      <div
+        class="hero-container"
+        :style="{ 'margin-top': heroContainerMargin + '%' }"
+      >
         <div class="text-center flex column layout-align-center-start">
-          <div class="hero" :style="{'opacity': heroContainerOpacity}">
-            <div class="profile-image m-auto"></div>
-            <div class="flex row layout-align-center-center m-t-10">
+          <div class="hero" :style="{ opacity: heroContainerOpacity }">
+            <div class="avatar m-auto"></div>
+            <div class="hero-title flex row layout-align-center-center m-t-10">
               <h1>Ido Postelnik</h1>
               <h2>Front End Engineer</h2>
             </div>
-            <div class="flex row layout-align-center-center m-t-5">
-              <h3><span class="m-r-10">I love doing UI.</span> <span class="m-r-10">From sketch to production.</span> <span>Simple as that.</span></h3>
+            <div class="hero-subtitle flex row layout-align-center-center m-t-5">
+              <h3>
+                <span class="m-r-10">I love doing UI.</span>
+                <span class="m-r-10">From sketch to production.</span>
+                <span>Simple as that.</span>
+              </h3>
             </div>
 
-            <button class="btn work-flow-button m-t-10" :class="{'active': shouldShowWorkFlowModes === true}" @click="toggleWorkFlowModes">{{workFlowButton}}</button>
+            <button
+              class="btn work-flow-button m-t-10"
+              :class="{ active: shouldShowWorkFlowModes === true }"
+              @click="toggleWorkFlowModes"
+            >
+              {{ workFlowButton }}
+            </button>
           </div>
           <!-- work flow process -->
-          <div class="work-flow flex column layout-align-center-center m-t-20" :class="{'active': shouldShowWorkFlowModes === true, 'on-scroll': shouldShowWorkFlowModesOnScroll === true}">
+          <div
+            class="work-flow flex column layout-align-center-center m-t-20"
+            :class="{
+              active: shouldShowWorkFlowModes === true,
+              'on-scroll': shouldShowWorkFlowModesOnScroll === true
+            }"
+          >
             <div class="steps flex layout-align-center-center">
-              <step v-for="(mode, key, index) in workFlowModes" 
-              :key="mode.title" 
-              :index="index + 1" 
-              :title="mode.title"
-              :isActive="mode.isActive"
-              :isLast="index === workFlowModesSize - 1" 
-              @click.native="switchWorkFlowMode(key)" 
-              class="m-r-10"></step>
+              <step
+                v-for="(mode, key, index) in workFlowModes"
+                :key="mode.title"
+                :index="index + 1"
+                :title="mode.title"
+                :isActive="mode.isActive"
+                :isLast="index === workFlowModesSize - 1"
+                @click.native="switchWorkFlowMode(key)"
+                class="m-r-10"
+              ></step>
             </div>
-            <img src="../assets/icons/close.svg" alt="close" class="close clickable" @click="hideWorkFlowModes"/>
+            <img
+              src="../assets/icons/close.svg"
+              alt="close"
+              class="close clickable"
+              @click="hideWorkFlowModes"
+            />
           </div>
-
         </div>
       </div>
-
     </div>
 
     <!-- inner page cards -->
     <div class="flex row layout-align-start-space-between m-t-20">
-      <inner-page-card v-for="page in PAGES" :key="page.value" :data="page"> </inner-page-card>
+      <inner-page-card
+        v-for="page in PAGES"
+        :key="page.value"
+        :data="page"></inner-page-card>
     </div>
 
     <!-- quote -->
@@ -60,47 +94,47 @@
     <!-- Footer -->
     <contact-footer></contact-footer>
 
-    <copy-right/>
+    <copy-right />
   </div>
 </template>
 
 <script>
-import { PAGES } from '../utils/constants';
-import {_} from '@/utils/utils';
+import { PAGES } from "../utils/constants";
+import { _ } from "@/utils/utils";
 
 const WORK_FLOW_MODES = {
   SKETCH: {
-    title: 'Sketch',
+    title: "Sketch",
     isActive: false
   },
   WIREFRAME: {
-    title: 'Wireframe',
+    title: "Wireframe",
     isActive: false
   },
   CODE: {
-    title: 'Code',
+    title: "Code",
     isActive: false
   },
   PRODUCTION: {
-    title: 'Production',
+    title: "Production",
     isActive: false
   }
 };
-const DEFAULT_ACTIVE_WORK_FLOW_MODE = 'PRODUCTION';
+const DEFAULT_ACTIVE_WORK_FLOW_MODE = "PRODUCTION";
 const WORK_FLOW_BUTTON = {
-  OFF: 'Check it out!',
-  ON: 'Back to normal'
+  OFF: "Check it out!",
+  ON: "Back to normal"
 };
 
 // @ is an alias to /src
-import InnerPageCard from '@/components/home/InnerPageCard.vue'
-import Step from '@/components/home/Step.vue'
-import ContactFooter from '@/components/home/ContactFooter.vue'
-import CopyRight from '@/components/CopyRight.vue'
+import InnerPageCard from "@/components/home/InnerPageCard.vue";
+import Step from "@/components/home/Step.vue";
+import ContactFooter from "@/components/home/ContactFooter.vue";
+import CopyRight from "@/components/CopyRight.vue";
 
 export default {
-  name: 'home',
-  data: function () {
+  name: "home",
+  data: function() {
     return {
       heroContainerMargin: 10,
       heroContainerOpacity: 1,
@@ -114,7 +148,7 @@ export default {
       userHadScrolled: false,
       //
       PAGES: PAGES
-    }
+    };
   },
   components: {
     Step,
@@ -123,10 +157,10 @@ export default {
     CopyRight
   },
   mounted() {
-    window.addEventListener('scroll', this.onScroll);
+    window.addEventListener("scroll", this.onScroll);
   },
-  beforeDestroy () {
-    window.removeEventListener('scroll', this.onScroll)
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
   },
   created() {
     this.setDefaultActiveWorkFlowMode();
@@ -136,70 +170,71 @@ export default {
       let windowInnerHeight = window.innerHeight;
       window.scrollTo({
         top: windowInnerHeight - 50,
-        behavior: 'smooth',
+        behavior: "smooth"
       });
     },
     //#region WorkFlow modes
-      setDefaultActiveWorkFlowMode() {
-        this.workFlowModes[this.activeWorkFlowMode].isActive = true;
-      },
-      toggleWorkFlowModes() {
-        if(this.shouldShowWorkFlowModes === true) {
-          this.shouldShowWorkFlowModes = false;
-          this.workFlowButton = WORK_FLOW_BUTTON.OFF;
-        }
-        else {
-          this.shouldShowWorkFlowModes = true;
-          this.workFlowButton = WORK_FLOW_BUTTON.ON;
-
-          // Active mode state
-          this.workFlowModes[this.activeWorkFlowMode].isActive = false;
-          this.workFlowModes[DEFAULT_ACTIVE_WORK_FLOW_MODE].isActive = true;
-          this.activeWorkFlowMode = DEFAULT_ACTIVE_WORK_FLOW_MODE;
-        }
-      },
-      hideWorkFlowModes() {
+    setDefaultActiveWorkFlowMode() {
+      this.workFlowModes[this.activeWorkFlowMode].isActive = true;
+    },
+    toggleWorkFlowModes() {
+      if (this.shouldShowWorkFlowModes === true) {
         this.shouldShowWorkFlowModes = false;
-        this.userHadScrolled = false;
-        this.shouldShowWorkFlowModesOnScroll = false;
         this.workFlowButton = WORK_FLOW_BUTTON.OFF;
+      } else {
+        this.shouldShowWorkFlowModes = true;
+        this.workFlowButton = WORK_FLOW_BUTTON.ON;
 
         // Active mode state
         this.workFlowModes[this.activeWorkFlowMode].isActive = false;
         this.workFlowModes[DEFAULT_ACTIVE_WORK_FLOW_MODE].isActive = true;
         this.activeWorkFlowMode = DEFAULT_ACTIVE_WORK_FLOW_MODE;
-      },
-      switchWorkFlowMode(modeKey) {
-        if(modeKey !== this.activeWorkFlowMode) {
-          this.workFlowModes[this.activeWorkFlowMode].isActive = false;
-          this.workFlowModes[modeKey].isActive = true;
-          this.activeWorkFlowMode = modeKey;
-        }
-      },
-      onScroll() {
-        if(this.userHadScrolled === false && this.shouldShowWorkFlowModes === true){
-          this.userHadScrolled = true;
-          this.shouldShowWorkFlowModesOnScroll = true;
-        }
-
-        let scrollPosition = window.scrollY;
-        this.heroContainerMargin = 10 + (scrollPosition / 50);
-
-        // if(scrollPosition > 200) {
-          this.heroContainerOpacity = 200 / scrollPosition;
-        // }
       }
+    },
+    hideWorkFlowModes() {
+      this.shouldShowWorkFlowModes = false;
+      this.userHadScrolled = false;
+      this.shouldShowWorkFlowModesOnScroll = false;
+      this.workFlowButton = WORK_FLOW_BUTTON.OFF;
+
+      // Active mode state
+      this.workFlowModes[this.activeWorkFlowMode].isActive = false;
+      this.workFlowModes[DEFAULT_ACTIVE_WORK_FLOW_MODE].isActive = true;
+      this.activeWorkFlowMode = DEFAULT_ACTIVE_WORK_FLOW_MODE;
+    },
+    switchWorkFlowMode(modeKey) {
+      if (modeKey !== this.activeWorkFlowMode) {
+        this.workFlowModes[this.activeWorkFlowMode].isActive = false;
+        this.workFlowModes[modeKey].isActive = true;
+        this.activeWorkFlowMode = modeKey;
+      }
+    },
+    onScroll() {
+      if (
+        this.userHadScrolled === false &&
+        this.shouldShowWorkFlowModes === true
+      ) {
+        this.userHadScrolled = true;
+        this.shouldShowWorkFlowModesOnScroll = true;
+      }
+
+      let scrollPosition = window.scrollY;
+      this.heroContainerMargin = 10 + scrollPosition / 50;
+
+      // if(scrollPosition > 200) {
+      this.heroContainerOpacity = 200 / scrollPosition;
+      // }
+    }
     //#endregion
   },
-  computed: {
-  }
-}
+  computed: {}
+};
 </script>
 
 <style scoped lang="scss">
 @import "@/styles/_mixins.scss";
 
-.home{
+.home {
   background: white;
 
   .main-container {
@@ -208,7 +243,7 @@ export default {
     background: $light-grey-l;
     background: linear-gradient(0deg, $light-grey-l 0%, $white 100%);
 
-    .sky{
+    .sky {
       position: absolute;
       top: 0;
       left: 0;
@@ -218,7 +253,7 @@ export default {
       background-image: url(../assets/img/home/road/sky.svg);
     }
 
-    .sun{
+    .sun {
       background-image: url(../assets/img/home/road/sun.svg);
       position: absolute;
       bottom: 0;
@@ -229,7 +264,7 @@ export default {
       background-position: bottom;
     }
 
-    .road{
+    .road {
       background-image: url(../assets/img/home/road/road.svg);
       position: absolute;
       bottom: 0;
@@ -242,7 +277,7 @@ export default {
       pointer-events: none;
     }
 
-    .arrow-down{
+    .arrow-down {
       position: absolute;
       bottom: 30px;
       transition: bottom 0.2s;
@@ -253,19 +288,19 @@ export default {
       filter: $filter-dark-grey;
       z-index: 5;
 
-      &:hover{
+      &:hover {
         bottom: 25px;
       }
     }
 
-    .hero-container{
+    .hero-container {
       width: 100%;
       margin-top: 10%;
-      
 
-      .hero{
+      .hero {
         z-index: 2;
-        .profile-image{
+
+        .avatar {
           height: calc(60px + 7.5vw);
           width: calc(60px + 7.5vw);
           border-radius: 50%;
@@ -274,9 +309,9 @@ export default {
           border: $light-grey 1px solid;
         }
 
-        h1{
-          font-size: calc(0.9rem + 2.0vw);
-          line-height: calc(0.9rem + 2.0vw);
+        h1 {
+          font-size: calc(0.9rem + 2vw);
+          line-height: calc(0.9rem + 2vw);
           font-weight: 600;
           font-family: $font-title;
           padding-right: 20px;
@@ -284,25 +319,25 @@ export default {
           border-right: 4px solid $dark-grey;
         }
 
-        h2{
+        h2 {
           font-size: calc(0.9rem + 1.3vw);
           letter-spacing: 1.5px;
           font-weight: 500;
-        } 
+        }
 
-        h3{
+        h3 {
           font-size: calc(0.75rem + 0.75vw);
           font-weight: 300;
 
-          span{
+          span {
             letter-spacing: 1.2px;
           }
         }
-        
-        .work-flow-button{
+
+        .work-flow-button {
           transition: all 0.2s;
 
-          &.active{
+          &.active {
             background-color: $green;
             color: $white;
           }
@@ -311,7 +346,7 @@ export default {
     }
   }
 
-  .work-flow{
+  .work-flow {
     display: none;
     border: 1px solid #313638;
     border-radius: 20px;
@@ -321,7 +356,7 @@ export default {
     background: white;
     padding: 18px 15px;
 
-    &.active{
+    &.active {
       position: relative;
       display: block;
       animation: fade-in 0.2s;
@@ -336,7 +371,7 @@ export default {
       }
     }
 
-    &.on-scroll{
+    &.on-scroll {
       position: fixed;
       top: calc(100vh - 135px);
       left: calc(50% - 300px);
@@ -352,7 +387,7 @@ export default {
       }
     }
 
-    .close{
+    .close {
       position: absolute;
       top: 10px;
       right: 10px;
@@ -371,7 +406,7 @@ export default {
   //       font-weight: 300;
   //       letter-spacing: 3px;
   //       z-index: 2;
-  //     }   
+  //     }
 
   //     .steps{
   //       width: 100%;
@@ -380,10 +415,75 @@ export default {
 
   //   }
   // }
-  
-  .quote{
+
+  .quote {
     height: 300px;
     font-size: calc(0.7rem + 0.9vw);
+  }
+
+  &.sketch-mode{
+    background: none;
+    background-image: url(../assets/img/home/workFlowModes/sketch/sketch-bg.svg);
+
+    .main-container{
+      background: none;
+      .sky{
+        top: 50px;
+        background-image: url(../assets/img/home/workFlowModes/sketch/sketch-sky.svg);
+      }
+
+      .sun{
+        display: none;
+      }
+      
+      .road{
+        background-image: url(../assets/img/home/workFlowModes/sketch/sketch-road.svg);
+      }
+    }
+
+    .hero-container {
+      margin-top: 15% !important;
+
+      .hero{
+        .avatar{
+          background-image: url(../assets/img/home/workFlowModes/sketch/sketch-avatar.svg);
+          background-size: contain;
+          background-repeat: no-repeat;
+          height: calc(60px + 5.5vw);
+        }
+
+        .hero-title{
+          background-image: url(../assets/img/home/workFlowModes/sketch/sketch-title.svg);
+          height: 80px;
+          width: 30vw;
+          background-size: contain;
+          background-repeat: no-repeat;
+          background-position: center;
+
+          h1, h2{
+            display: none;
+          }
+        }
+        
+        .hero-subtitle{
+          h3, span{
+            display: none;
+          }
+        }
+
+        .work-flow-button{
+          background-color: transparent !important;
+          color: transparent !important;
+          border: none;
+          background-image: url(../assets/img/home/workFlowModes/sketch/sketch-button.svg);
+          background-size: contain;
+          background-repeat: no-repeat;
+          background-position: center;
+          height: 60px;
+        }
+      }
+    }
+
   }
 }
 </style>
